99a2c1a3fe37b661b84e53a4340b14d7c38f73c7
Aiden Berzins <aiden.berzins@gmail.com>
2023-12-08 14:01:15 -0500

    adding typescript, tailwind, and react to astro config

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        wave: {
+          '0%, 100%': { transform: 'rotate(-6deg)' },
+          '50%': { transform: 'rotate(6deg)' },
+        },
+      },
+      animation: {
+        wave: 'wave 1s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [],
+};
